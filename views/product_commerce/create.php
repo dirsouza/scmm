cba4a407ae5d92c5fb2d58e29531ef641e0eae89
Diogo Souza <diogo.souza@msn.com>
2018-05-04 14:06:15 -0400

    Trabalhando no Produtos por Comercios

--- views/product_commerce/create.php
+++ views/product_commerce/create.php
@@ -14,56 +14,50 @@
                             <div class="panel-body">
                                 <div class="row">
                                     <div class="col-md-12">
-                                        <form action="/scmm/registration/product/create" method="POST">
-                                            <div class="col-md-7">
-                                                <div class="form-group">
-                                                    <label>Comércio:</label>
-                                                    <select name="idComercio" class="form-control select2" autofocus>
-                                                        <option></option>
-                                                    <?php foreach ($commerces as $key): ?>
-                                                        <option value="<?= $key['idcomercio'] ?>"><?= $key['desnome'] ?></option>
-                                                    <?php endforeach; ?>
-                                                    </select>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Comércio:</label>
+                                                <select name="idComercio" class="form-control select2" autofocus>
+                                                    <option></option>
+                                                <?php foreach ($commerces as $key): ?>
+                                                    <option value="<?= $key['idcomercio'] ?>"><?= $key['desnome'] ?></option>
+                                                <?php endforeach; ?>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="panel panel-danger">
+                                                <div class="panel-heading text-center">
+                                                    Lista de Produtos
+                                                </div>
+                                                <div class="panel-body">
+                                                    <table id='table' class="table table-bordered table-responsive table-striped table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th width="10%">Código</th>
+                                                                <th>Nome</th>
+                                                                <th width="30%">Marca</th>
+                                                                <th width="10%">Preço</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <?php foreach ($products as $key) : ?>
+                                                            <tr>
+                                                                <td class="text-center"><?= str_pad($key['idproduto'], 5, 0, STR_PAD_LEFT)?></td>
+                                                                <td><?= $key['desnome'] ?></td>
+                                                                <td><?= $key['desmarca'] ?></td>
+                                                                <td style="margin: auto; padding: 3px;"><input type="text" name="preco[]" id="preco" class="form-control input-sm" style="width: 100%"></td>
+                                                            </tr>
+                                                            <?php endforeach; ?>
+                                                        </tbody>
+                                                    </table>
                                                 </div>
                                             </div>
-                                            <div class="col-md-5">
-                                                <label>Produtos:</label>
-                                                <div class="form-group input-group">
-                                                    <select name="idProduto" class="form-control select2">
-                                                        <option></option>
-                                                    <?php foreach ($products as $key) : ?>
-                                                        <option value="<?= $key['idproduto'] ?>"><?= $key['desnome'] . " - " . $key['desmarca'] ?></option>
-                                                    <?php endforeach; ?>
-                                                    </select>
-                                                    <span class="input-group-btn">
-                                                        <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Adicionar"><i class="glyphicon glyphicon-plus"></i></button>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-12">
-                                                <div class="panel panel-danger">
-                                                    <div class="panel-heading text-center">
-                                                        Lista de Produtos
-                                                    </div>
-                                                    <div class="panel-body">
-                                                        <table class="table table-bordered table-responsive table-striped table-hover">
-                                                            <thead>
-                                                                <tr>
-                                                                    <th>Código</th>
-                                                                    <th>Nome</th>
-                                                                    <th>Marca</th>
-                                                                    <th>Preço</th>
-                                                                </tr>
-                                                            </thead>
-                                                        </table>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="text-right col-md-12">
-                                                <button type="submit" class="btn btn-success">Cadastrar</button>
-                                                <button type="button" class="btn btn-warning" onclick="javascript: location.href='/scmm/registration/prodsByCommerce'">Cancelar</button>
-                                            </div>
-                                        </form>
+                                        </div>
+                                        <div class="text-right col-md-12">
+                                            <button type="button" id="btn-prodsByCommerce" class="btn btn-success">Cadastrar</button>
+                                            <button type="button" class="btn btn-warning" onclick="javascript: location.href='/scmm/registration/prodsByCommerce'">Cancelar</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
